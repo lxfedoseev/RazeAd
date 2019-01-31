854987f227d69e8ac608b2dab028d4b2d33b6887
Alex Fedoseev <lxfedoseev@gmail.com>
2019-01-31 20:32:51 +0300

    AR chapter 10

--- RazeAd/ui/AdViewController.swift
+++ RazeAd/ui/AdViewController.swift
@@ -31,6 +31,7 @@
 import UIKit
 import SceneKit
 import ARKit
+import Vision
 
 class AdViewController: UIViewController {
   @IBOutlet var sceneView: ARSCNView!
@@ -83,6 +84,27 @@ class AdViewController: UIViewController {
 
 // MARK: - ARSCNViewDelegate
 extension AdViewController: ARSCNViewDelegate {
+    
+    func renderer(_ renderer: SCNSceneRenderer,
+                  nodeFor anchor: ARAnchor) -> SCNNode? {
+        // 1
+        guard let billboard = billboard else { return nil }
+        var node: SCNNode? = nil
+        // 2
+        //DispatchQueue.main.sync {
+        switch anchor {
+        // 3
+        case billboard.billboardAnchor:
+            let billboardNode = addBillboardNode()
+            node = billboardNode
+        default:
+            break
+        }
+        //}
+        return node
+    }
+    
+    
 }
 
 extension AdViewController: ARSessionDelegate {
@@ -90,6 +112,7 @@ extension AdViewController: ARSessionDelegate {
   }
 
   func sessionWasInterrupted(_ session: ARSession) {
+    removeBillboard()
   }
 
   func sessionInterruptionEnded(_ session: ARSession) {
@@ -98,8 +121,120 @@ extension AdViewController: ARSessionDelegate {
 
 extension AdViewController {
   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    // 1
+    guard let currentFrame = sceneView.session.currentFrame else {
+        return
+    }
+    // 2
+    DispatchQueue.global(qos: .background).async {
+        // 3
+        do { // 4
+            let request =
+                VNDetectRectanglesRequest {(request, error) in
+                    // Access the first result in the array,
+                    // after converting to an array
+                    // of VNRectangleObservation
+                    // 5
+                    guard
+                        let results = request.results?
+                            .compactMap({ $0 as? VNRectangleObservation }),
+                        // 6
+                        let result = results.first else {
+                            print ("[Vision] VNRequest produced no result")
+                            return
+                    }
+                    // 1
+                    let coordinates: [matrix_float4x4] = [
+                        result.topLeft,
+                        result.topRight,
+                        result.bottomRight,
+                        result.bottomLeft
+                        ].compactMap {
+                            // 2
+                            guard let hitFeature = currentFrame.hitTest(
+                                $0, types: .featurePoint).first else { return nil }
+                            // 3
+                            return hitFeature.worldTransform
+                    }
+                    // 4
+                    guard coordinates.count == 4 else { return }
+                    // 5
+                    DispatchQueue.main.async {
+                        // 6
+                        self.removeBillboard()
+                        let (topLeft, topRight, bottomRight, bottomLeft) =
+                            (coordinates[0], coordinates[1],
+                             coordinates[2], coordinates[3])
+                        // 7
+                        self.createBillboard(topLeft: topLeft, topRight: topRight,
+                                             bottomRight: bottomRight, bottomLeft: bottomLeft)
+                        for coordinate in coordinates {
+                            // 1
+                            let box = SCNBox(width: 0.01, height: 0.01,
+                                             length: 0.001, chamferRadius: 0.0)
+                            // 2
+                            let node = SCNNode(geometry: box)
+                            // 3
+                            node.transform = SCNMatrix4(coordinate)
+                            // 4
+                            self.sceneView.scene.rootNode.addChildNode(node)
+                        }
+                    }
+                    
+            }
+            // 1
+            let handler = VNImageRequestHandler(
+                cvPixelBuffer: currentFrame.capturedImage)
+            // 2
+            try handler.perform([request])
+            
+        } catch(let error) {
+            print(
+                "An error occurred during rectangle detection: \(error)")
+        }
+    }
   }
 }
 
 private extension AdViewController {
+    
+    func createBillboard(
+        topLeft: matrix_float4x4, topRight: matrix_float4x4,
+        bottomRight: matrix_float4x4, bottomLeft: matrix_float4x4) {
+        // 1
+        let plane = RectangularPlane(
+            topLeft: topLeft, topRight: topRight,
+            bottomLeft: bottomLeft, bottomRight: bottomRight)
+        // 2
+        let anchor = ARAnchor(transform: plane.center)
+        // 3
+        billboard =
+            BillboardContainer(billboardAnchor: anchor, plane: plane)
+        // 4
+        sceneView.session.add(anchor: anchor)
+        print("New billboard created")
+    }
+    
+    func addBillboardNode() -> SCNNode? {
+        guard let billboard = billboard else { return nil }
+        // 1
+        let rectangle = SCNPlane(width: billboard.plane.width,
+                                 height: billboard.plane.height)
+        // 2
+        let rectangleNode = SCNNode(geometry: rectangle)
+        self.billboard?.billboardNode = rectangleNode
+        return rectangleNode
+    }
+    
+    func removeBillboard() {
+        // 1
+        if let anchor = billboard?.billboardAnchor {
+            // 2
+            sceneView.session.remove(anchor: anchor)
+            // 3
+            billboard?.billboardNode?.removeFromParentNode()
+            billboard = nil
+        }
+    }
+    
 }
